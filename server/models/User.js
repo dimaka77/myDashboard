687fe141dbd0508c3648edf59c2742c6152a132b
DR <dimaka77@gmail.com>
2020-03-07 12:56:38 -0800

    setup passport and user schema

---
+++ server/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { Schema } = mongoose;
+
+let userschema = new Schema({
+  email: String,
+  password: String
+});
+
+userschema.methods.generateHash = password => {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+};
+
+userschema.methods.validaPassword = password => {
+  return bcrypt.compareSync(password, this.password);
+};
+
+let User = mongoose.model('User', userschema);
+
+module.exports = User;

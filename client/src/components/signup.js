f2abcdd2f4cc4ea315210dc4d65a15322987c897
AlexTolstikhin <alexeytolstikhin@gmail.com>
2020-03-07 14:42:42 -0800

    add initial form

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -3,7 +3,23 @@ import { Field, reduxForm } from 'redux-form';
 
 const SignUp = props => {
     const { handleSubmit } = props;
-    return <form onSubmit={handleSubmit}>TEST{/* form body*/}</form>;
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="firstName">First Name</label>
+                <Field name="firstName" component="input" type="text" />
+            </div>
+            <div>
+                <label htmlFor="lastName">Last Name</label>
+                <Field name="lastName" component="input" type="text" />
+            </div>
+            <div>
+                <label htmlFor="email">Email</label>
+                <Field name="email" component="input" type="email" />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
 
 export default reduxForm({ form: 'singup' })(SignUp);

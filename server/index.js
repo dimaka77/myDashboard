f0ceab88b8c590327ae54e3f8e2bed65cde26085
DR <dimaka77@gmail.com>
2020-03-07 16:03:53 -0800

    added proxy to serbe react app in prod

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -31,6 +32,16 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 app.use(flash());
 
 require('./models/User');

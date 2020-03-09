8c8bfdd65322980b23e603bafcd76c631c26582a
AlexTolstikhin <alexeytolstikhin@gmail.com>
2020-03-08 22:19:55 -0700

    styling signup

--- client/src/components/home/smart/Signup.css.js
+++ client/src/components/home/smart/Signup.css.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
 export const InputForm = styled.form`
-    width: 200px;
-    height: 200px;
 `;
 
 export const InputFormWrapper = styled.div`
@@ -11,3 +9,45 @@ export const InputFormWrapper = styled.div`
     align-items: center;
     min-height: 100vh;
 `;
+
+export const Title = styled.h1`
+    color: #2556DA;
+    text-align: center;
+`
+
+export const TextInput = styled.input`
+    min-height: 20px;
+    width: 200px;
+    margin: 10px 0 10px 10px;
+`
+
+export const TextInputWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const Label = styled.label`
+    color: #2556DA;
+`
+
+export const Button = styled.button`
+    color: #2556DA;
+    align-self: flex-end;
+    height: 30px;
+    width: 80px;
+    border-radius: 10px;
+    border: 2px solid #2556DA;
+    :focus {
+        outline: none;
+    }
+    :active {
+        border-style: initial;
+        border: 2px solid #DA2559;
+        color: #DA2559;
+    }
+`
+
+export const ButtonWrapper = styled(TextInputWrapper)`
+    justify-content: flex-end;
+`
--- client/src/components/home/smart/Signup.jsx
+++ client/src/components/home/smart/Signup.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
-import { InputForm, InputFormWrapper } from './Signup.css';
+import {
+	Button,
+	Label, 
+	InputForm,
+	InputFormWrapper, 
+	Title, 
+	TextInput,
+	TextInputWrapper,
+	ButtonWrapper
+} from './Signup.css';
 
 import { Field, reduxForm } from 'redux-form';
 
 const SignUp = props => {
-  const { handleSubmit } = props;
-  return (
-    <InputFormWrapper>
-      <InputForm onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="firstName">First Name</label>
-          <Field name="firstName" component="input" type="text" />
-        </div>
-        <div>
-          <label htmlFor="lastName">Last Name</label>
-          <Field name="lastName" component="input" type="text" />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <Field name="email" component="input" type="email" />
-        </div>
-        <button type="submit">Submit</button>
-      </InputForm>
-    </InputFormWrapper>
-  );
+	const { handleSubmit } = props;
+	return (
+		<InputFormWrapper>
+			<InputForm onSubmit={handleSubmit}>
+				<Title>Sign Up</Title>
+				<TextInputWrapper>
+					<Label>First name:</Label>
+					<Field name="firstName" component={TextInput} type="text" />
+				</TextInputWrapper>
+				<TextInputWrapper>
+					<Label>Last name:</Label>
+					<Field name="lastName" component={TextInput} type="text" />
+				</TextInputWrapper>
+				<TextInputWrapper>
+					<Label>Email:</Label>
+					<Field name="email" component={TextInput} type="email" />
+				</TextInputWrapper>
+				<ButtonWrapper>
+					<Button type="submit">Submit</Button>
+				</ButtonWrapper>
+			</InputForm>
+		</InputFormWrapper>
+	);
 };
 
 export default reduxForm({ form: 'signup' })(SignUp);

69cda87cc92a2981d5bd1733c51b0562ece4d137
AlexTolstikhin <alexeytolstikhin@gmail.com>
2020-03-14 15:55:01 -0700

    added proxy to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,45 +1,42 @@
 const path = require('path');
 
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
+    mode: 'development',
+    entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
-        publicPath: "/public/"
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/public/'
     },
     resolve: {
-        modules: [
-            "node_modules"
-        ],
-        extensions: [".js", ".json", ".jsx", ".css", "*"]
-    }, 
+        modules: ['node_modules'],
+        extensions: ['.js', '.json', '.jsx', '.css', '*']
+    },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules)/,
                 use: {
-                    loader: "babel-loader"
+                    loader: 'babel-loader'
                 }
             },
             {
                 test: /\.html$/,
                 use: [
                     {
-                        loader: "html-loader"
+                        loader: 'html-loader'
                     }
                 ]
             },
             {
                 test: /\.css$/,
                 use: [
-                    "style-loader",
+                    'style-loader',
                     {
-                        loader: "css-loader",
+                        loader: 'css-loader',
                         options: {
                             modules: true
                         }
@@ -48,21 +45,27 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|gif|ico)$/,
-                use: ["file-loader"]
+                use: ['file-loader']
             }
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
-            template: "./public/index.html",
-            filename: "./index.html"
+            template: './public/index.html',
+            filename: './index.html'
         })
     ],
     devServer: {
-        contentBase: path.join(__dirname, "dist"),
+        contentBase: path.join(__dirname, 'dist'),
         compress: true,
         port: 3000,
         watchContentBase: true,
-        progress: true
+        progress: true,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5000',
+                success: false
+            }
+        }
     }
-}
+};
